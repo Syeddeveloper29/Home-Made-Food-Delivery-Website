c739100d07a003fc0a63c33cda0b97d610978d4b
Syed Mehdi <158597106+Syeddeveloper29@users.noreply.github.com>
2025-06-17 04:50:20 +0500

    Updated Files On 17 June 2025

--- Home Made Food Delivery Website/js/function.js
+++ Home Made Food Delivery Website/js/function.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector('#navbar');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     if (window.scrollY >= 50) {
         navbar.classList.add('sticky');
     } else {
@@ -16,12 +16,12 @@ const icon = document.querySelector('.fa-bars');
 burger.addEventListener('click', () => {
     const isOpen = navbar.classList.contains('active');
 
-    if(!isOpen){
+    if (!isOpen) {
         navbar.classList.add('active');
         icon.classList.replace('fa-bars', 'fa-xmark');
-    }else{
+    } else {
         navbar.classList.remove('active');
-        setTimeout( () => {
+        setTimeout(() => {
             icon.classList.replace('fa-xmark', 'fa-bars');
         }, 400);
     }
@@ -32,12 +32,31 @@ burger.addEventListener('click', () => {
 const subscribeHeading = document.querySelector('#subscribe-heading');
 
 const changeSubscribeHeading = () => {
-    if(window.innerWidth <= 478){
+    if (window.innerWidth <= 478) {
         subscribeHeading.textContent = `Stay Hungry for More!`;
-    }else{
+    } else {
         subscribeHeading.textContent = `🍽️ Stay Hungry for More!`
     }
 }
 
 window.addEventListener('load', changeSubscribeHeading);
 window.addEventListener('resize', changeSubscribeHeading);
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw2xyA1na2uifS_w1QnmJXV8OG9iY2zoMUpxl7W_G0dJBZK9NpcbaarK1R4DbuKcq1sNQ/exec';
+const form = document.forms['submit-to-google-sheet'];
+const subscribeMessage = document.querySelector('.subscribe-last-text');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            subscribeMessage.textContent = `Thank you for subscribing!`;
+            setTimeout(() => {
+                subscribeMessage.textContent = ``;
+                form.reset();
+            }, 5000);
+        })
+        .catch(error => console.error('Error!', error.message));
+});
